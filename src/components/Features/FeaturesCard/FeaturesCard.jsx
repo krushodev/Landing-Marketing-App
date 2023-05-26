5ac1f5baf80e24e1d4cd18e837961f7c5219b4aa
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-05-26 16:16:43 -0300

    feat: add Animation to FeaturesCard

--- src/components/Features/FeaturesCard/FeaturesCard.jsx
+++ src/components/Features/FeaturesCard/FeaturesCard.jsx
@@ -1,22 +1,25 @@
 import "./FeaturesCard.css";
+import { motion } from "framer-motion";
 
 const FeaturesCard = ({ content }) => {
   return (
-    <div className="features-card">
-      <img src={content.img} alt={content.id} />
-      <div className="features-card-content">
-        <h5>{content.title}</h5>
-        <p>{content.desc}</p>
-        <ul>
-          {content.feat.map((item, index) => (
-            <li key={index}>
-              <span>{item}</span>
-            </li>
-          ))}
-        </ul>
-        <button className="btn">LightBox</button>
+    <motion.div key={`${content.id}-card`} initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.8}}>
+      <div className="features-card">
+        <img src={content.img} alt={content.id} />
+        <div className="features-card-content">
+          <h5>{content.title}</h5>
+          <p>{content.desc}</p>
+          <ul>
+            {content.feat.map((item, index) => (
+              <li key={index}>
+                <span>{item}</span>
+              </li>
+            ))}
+          </ul>
+          <button className="btn">LightBox</button>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
